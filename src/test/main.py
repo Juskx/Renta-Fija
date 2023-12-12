b8388619a252e2c968c45ea50c3adba192622d49
Joaquin Pulgar <joaquin.pulgar10@gmail.com>
2023-12-12 17:50:45 -0300

    d

--- src/test/main.py
+++ src/test/main.py
@@ -1,4 +1,4 @@
-### main entregable 3 Derivados renta fija: Tomás Barría, Francisco Gorigoitía, Lucas Laragit config --global user.name "FIRST_NAME LAST_NAME"
+### ENTREGABLE NÚMERO 3 MAIN FILE###
 import sys
 import os
 
@@ -9,11 +9,10 @@ from datetime import date
 from datetime import datetime
 from package.funcs.functions import get_ufs
 
-# Ejemplo uf diciembre 9 2023 al 9 de enero 2024
-last_uf_day_9 = 36607.69  # El valor de la UF para el día 9 del último mes
-ipc_day_9 = 0.007  # El IPC para el día 9 del último mes
-date_last_uf = datetime(2023, 12, 9)  # Fecha inicial
+last_uf_day_9 = 36607.69  
+ipc_day_9 = 0.007  
+date_last_uf = datetime(2023, 12, 9)  
 
-# Calcular los valores de la UF hasta el próximo 9 del proximo mes:
+
 valor_uf = get_ufs(date_last_uf, last_uf_day_9, ipc_day_9)
 print(valor_uf["09-01-2024"])
