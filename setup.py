70729b8f260e9bbf8e7ff42ff58119b07bee5731
Joaquin Pulgar <joaquin.pulgar10@gmail.com>
2023-12-12 17:30:32 -0300

    s

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="entregable_3",
+    version="0.1.1",
+    author="Joaquin Pulgar",
+    author_email="joaquin.pulgar10@gmail.com",
+    description="Test",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Juskx/Renta-Fija",
+    license="GPL-3.0-or-later",  # Actualicé la licencia para que coincida con el código SPDX
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src", exclude=[""]),
+    python_requires=">=3.10",
+    install_requires=["dateutil", "datetime", "calendar"],
+)
